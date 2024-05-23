c7b52c90c061307024d8a61977f2211e9d89f4f5
Adrián Martínez Fuentes <UO295454@uniovi.es>
2024-05-23 11:51:09 +0200

    Creating the users router

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const jwt = require("jsonwebtoken"); 
+const database = require("./database")
+const activeApiKeys = require("./activeApiKeys")
 
-app.get('/', (req, res) => {
-  res.send('Hello world!');
-});
+const port = 4000
+const app = express()
+var cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+
+app.use(["",""] /* TO-DO */ ,(req,res,next)=>{
+	console.log("middleware execution")
+
+	let apiKey = req.query.apiKey
+	if ( apiKey == undefined ){
+		res.status(401).json({ error: "no apiKey" });
+		return 
+	}
+	let infoInApiKey = jwt.verify(apiKey, "secret");
+	if ( infoInApiKey == undefined || activeApiKeys.indexOf(apiKey) == -1){
+		res.status(401).json({ error: "invalid apiKey" });
+		return 	
+	}
+
+	// desencrypted in req
+	req.infoInApiKey = infoInApiKey;
+  next()
+})
 
 app.listen(port, () => {
-  console.log(`Executing server in port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
